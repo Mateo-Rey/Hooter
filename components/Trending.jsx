6705b19afbd37037ef6bcb7cd0a331592658a879
Mateo-Rey <jrey3darts@gmail.com>
2022-11-01 11:53:29 -0400

    no twitter

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -9,7 +9,7 @@ const Trending = () => {
 
             <div className='bg-[#16181C] flex gap-2 rounded-full py-2 px-4 text-white items-center text-[20px] sticky top-1 z-10'>
                 <FiSearch />
-                <input className='bg-transparent w-[100%] outline-none' type="text" placeholder='Search Twitter' />
+                <input className='bg-transparent w-[100%] outline-none' type="text" placeholder='Search Hooter' />
             </div>
 
 
